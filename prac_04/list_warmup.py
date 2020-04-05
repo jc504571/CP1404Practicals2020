5dd80ad188d8f77b64e68b850d6e5ac37337b0c5
Navjot Kaur <navjotkaur7@my.jcu.edu.au>
2020-04-05 12:08:46 +1000

    add listswarmup

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,26 @@
+"""
+CP1404 Practicals2020
+Lists "warm-up"
+"""
+
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]= 3
+# numbers[-1]=2
+# numbers[3]=1
+# numbers[:-1]
+# numbers[3:4]
+# 5 in numbers=True
+# 7 in numbers=False
+# "3" in numbers =False
+# print(numbers + [6, 5, 3]) =[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+numbers[0] = "ten" # Change the first element of numbers to "ten"
+
+numbers[-1] = 1  # Change the last element of numbers to 1
+
+numbers[2:]  # Get all the elements from numbers except the first two
+
+9 in numbers  # Check if 9 is an element of numbers
