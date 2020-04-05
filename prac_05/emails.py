17aeb1569183a640a6a363a7d66e2999d81ae530
Navjot Kaur <navjotkaur7@my.jcu.edu.au>
2020-04-05 16:53:30 +1000

    add emails

--- prac_05/emails.py
+++
@@ -1,7 +0,0 @@
-
-def main():
-    """Creating dictionary of emails-to-names."""
-    email_to_name = {}
-    email = input("Email: ")
-    while email != "":
-        name = get_name_from_email(email)
